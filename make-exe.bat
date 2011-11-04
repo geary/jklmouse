10745f030ee7d7f067b4ea57c4f3ddfd3b585363
Michael Geary <mg@mg.to>
2011-11-03 21:08:51 -0700

    Rearrange and add compiler and licenses

---
+++ make-exe.bat
@@ -0,0 +1,4 @@
+Compiler\Ahk2Exe /in Source\JKLmouse.ahk /out Binary\JKLmouse32a.exe /icon Source\JKLmouse.ico /bin "Compiler\ANSI 32-bit.bin"
+Compiler\Ahk2Exe /in Source\JKLmouse.ahk /out Binary\JKLmouse32.exe /icon Source\JKLmouse.ico /bin "Compiler\Unicode 32-bit.bin"
+Compiler\Ahk2Exe /in Source\JKLmouse.ahk /out Binary\JKLmouse64.exe /icon Source\JKLmouse.ico /bin "Compiler\Unicode 64-bit.bin"
+pause
