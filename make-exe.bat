b66233c5db2c44a6d76fc67004d0251536115b92
Michael Geary <mg@mg.to>
2011-11-04 00:26:50 -0700

    Rename Binary to Build

--- make-exe.bat
+++ make-exe.bat
@@ -1,4 +1,4 @@
-Compiler\Ahk2Exe /in Source\JKLmouse.ahk /out Binary\JKLmouse32a.exe /icon Source\JKLmouse.ico /bin "Compiler\ANSI 32-bit.bin"
-Compiler\Ahk2Exe /in Source\JKLmouse.ahk /out Binary\JKLmouse32.exe /icon Source\JKLmouse.ico /bin "Compiler\Unicode 32-bit.bin"
-Compiler\Ahk2Exe /in Source\JKLmouse.ahk /out Binary\JKLmouse64.exe /icon Source\JKLmouse.ico /bin "Compiler\Unicode 64-bit.bin"
+Compiler\Ahk2Exe /in Source\JKLmouse.ahk /out Build\JKLmouse32a.exe /icon Source\JKLmouse.ico /bin "Compiler\ANSI 32-bit.bin"
+Compiler\Ahk2Exe /in Source\JKLmouse.ahk /out Build\JKLmouse32.exe /icon Source\JKLmouse.ico /bin "Compiler\Unicode 32-bit.bin"
+Compiler\Ahk2Exe /in Source\JKLmouse.ahk /out Build\JKLmouse64.exe /icon Source\JKLmouse.ico /bin "Compiler\Unicode 64-bit.bin"
 pause
